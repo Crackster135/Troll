74f4817e770875dd18b18b937ff6a7bd52b3186d
Crackster135 <jekalemoeder420@gmail.com>
2024-07-10 01:16:36 +0200

    commit

--- events/browser.py
+++ events/browser.py
@@ -6,7 +6,7 @@ import webbrowser
 firefox_path = '/usr/bin/firefox'
 
 def open(n):
-    webbrowser.get(firefox_path).open('https://i.kym-cdn.com/entries/icons/facebook/000/040/160/smokingcaterpillarkrater.jpg')
-    # while n <= random.randint(20,200):
-    #     n+=1
-    #     time.sleep(random.uniform(0, 0.5))
+    while n <= random.randint(1,10):
+        webbrowser.get(firefox_path).open('https://i.kym-cdn.com/entries/icons/facebook/000/040/160/smokingcaterpillarkrater.jpg')
+        n+=1
+        time.sleep(random.uniform(0, 0.5))
