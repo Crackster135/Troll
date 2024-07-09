99d92119d5c20308701b661dcf096eb083ae0d5d
Crackster135 <137114218+Crackster135@users.noreply.github.com>
2024-07-09 23:15:34 +0200

    initial commit

---
+++ run.py
@@ -0,0 +1,26 @@
+import pyautogui, sys
+import time
+print('Press Ctrl-C to quit.')
+
+def countdown(n):
+    while n >= 1:
+        print(f"Time remaining: {n} second(s)")
+        time.sleep(1)
+        n -= 1
+    execute_function()
+
+def execute_function():
+    print("Executing function...")
+    pyautogui.moveTo(720, 720)
+    pyautogui.click(clicks=100)
+    try:
+        while True:
+            x, y = pyautogui.position()
+            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print('\n')
+
+# Start the countdown from 10
+countdown(10)
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY python3 /home/troll/Documents/Troll/run.py
